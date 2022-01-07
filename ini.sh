4035c00333ebae002252e0adbaafa20c06b843cd
RoberthCorrea <85997636+RoberthCorrea@users.noreply.github.com>
2022-01-07 17:10:44 -0300

    Update ini.sh

--- ini.sh
+++ ini.sh
@@ -12,7 +12,7 @@ sudo apt -y install jq
 echo "Instalando Go"
 
 wget https://go.dev/dl/go1.17.6.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.16.5.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.6.linux-amd64.tar.gz
 rm -rf go1.17.6.linux-amd64.tar.gz
 cd /usr/local/go/bin
 cp go /usr/local/bin
