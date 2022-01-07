f797c2c62e8bdee3949a309c9d86629abc9b615b
RoberthCorrea <85997636+RoberthCorrea@users.noreply.github.com>
2022-01-07 16:52:16 -0300

    Update ini.sh

--- ini.sh
+++ ini.sh
@@ -13,6 +13,7 @@ echo "Instalando Go"
 
 wget https://go.dev/dl/go1.17.6.linux-amd64.tar.gz
 tar -xvf go1.17.6.linux-amd64.tar.gz
+rm -rf go1.17.6.linux-amd64.tar.gz
 cd go && cd bin
 cp go /usr/local/bin
 echo ' ' >> $HOME/.bashrc
