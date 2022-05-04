87081fbd5cd31d650ab60cdebe623a780264c61a
RoberthCorrea <85997636+RoberthCorrea@users.noreply.github.com>
2022-05-04 11:29:59 -0300

    Update ini.sh

--- ini.sh
+++ ini.sh
@@ -18,9 +18,9 @@ wget -q https://raw.githubusercontent.com/m4ll0k/takeover/master/takeover.py
 
 echo "Instalando Go"
 
-wget https://go.dev/dl/go1.17.6.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.6.linux-amd64.tar.gz
-rm -rf go1.17.6.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.18.1.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.1.linux-amd64.tar.gz
+rm -rf go1.18.1.linux-amd64.tar.gz
 cd /usr/local/go/bin
 cp go /usr/local/bin
 echo ' ' >> $HOME/.bashrc
